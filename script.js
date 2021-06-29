87dd0f59353ea1c21db1a9e0ace6e6b1ff12e895
Andre <andryushka-frolov.0@yandex.ru>
2021-06-29 16:37:09 +0300

    ЭЭЭ ну бля вроде ток меню осталось

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').addEventListener('click', 
+function(){
+document.querySelector('.burger span').classList.toggle('active');
+document.querySelector('.menu').classList.toggle("animate");
+});
