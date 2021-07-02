e7fe152ebe22eab8d957c78be61b91e9b7bbf9a7
Andre <andryushka-frolov.0@yandex.ru>
2021-07-02 23:33:00 +0300

    Ну хотя бы пашет

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function(){
 document.querySelector('.burger span').classList.toggle('active');
 document.querySelector('.menu').classList.toggle("animate");
 });
+document.querySelector('.burger').addEventListener('click', 
+function(){
+document.querySelector('body').classList.toggle("ass");
+});
+document.querySelector('.burger').addEventListener('click', 
+function(){
+document.querySelector('.black').classList.toggle("blacker");
+});
+
